106ff9a041895bd3ab6f64fc3711617f31a84582
Nick Gowdy <nickgowdy87@gmail.com>
2023-04-19 09:57:29 +0100

    can talk to api

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/big"
+	"net/http"
+	"time"
 )
 
 const allowedChars = "ABCDEF0123456789"
+
+type Request struct {
+	Deveui string `json:"deveui"`
+}
 
 func main() {
 	hexStr, err := generateHexString(16)
@@ -17,6 +25,25 @@ func main() {
 	}
 
 	fmt.Print(hexStr)
+
+	client := http.Client{Timeout: time.Duration(time.Second * time.Duration(30))}
+	b := new(bytes.Buffer)
+
+	reqBody := Request{Deveui: hexStr}
+
+	err = json.NewEncoder(b).Encode(&reqBody)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	resp, err := client.Post("http://europe-west1-machinemax-dev-d524.cloudfunctions.net/sensor-onboarding-sample", "application/json", b)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	fmt.Print(resp)
 }
 
 func generateHexString(length int) (string, error) {

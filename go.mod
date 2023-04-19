615c9bb33f67b1430058e414858bcdd5994f6018
Nick Gowdy <nickgowdy87@gmail.com>
2023-04-19 17:33:11 +0100

    added env vars

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/NickGowdy/deveui-cli
 
 go 1.19
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/NickGowdy/deveui-cli/channel"
 	"github.com/NickGowdy/deveui-cli/client"
 	"github.com/NickGowdy/deveui-cli/processor"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load(".env")
+
+	baseurl := os.Getenv("BASE_URL")
+
+	timeout, err := strconv.Atoi(os.Getenv("TIMEOUT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	limit, err := strconv.Atoi(os.Getenv("CODE_REGISTRATION_LIMIT"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	codeChannel := &channel.CodeChannel{
 		Msgch:  make(chan channel.Message, 10),
@@ -19,15 +36,15 @@ func main() {
 	signalChannel := &channel.SignalChannel{}
 
 	client := &client.HttpClient{
-		BaseUrl: "http://europe-west1-machinemax-dev-d524.cloudfunctions.net",
-		Client:  http.Client{Timeout: time.Duration(time.Second * time.Duration(30000))},
+		BaseUrl: baseurl,
+		Client:  http.Client{Timeout: time.Duration(time.Second * time.Duration(timeout))},
 	}
 
 	CodeProcessor := &processor.CodeProcessor{
 		Client:         client,
 		CodeChannel:    codeChannel,
 		SignalChannel:  signalChannel,
-		RegisterNumber: 10,
+		RegisterNumber: limit,
 	}
 
 	CodeProcessor.Start()

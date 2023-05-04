ec15adcd69267a027f0625568c1b426679328585
Nick Gowdy <nickgowdy87@gmail.com>
2023-05-04 13:31:44 +0100

    Move interface to lorawan_client.go

--- client/lorawan_client.go
+++ client/lorawan_client.go
@@ -6,6 +6,11 @@ import (
 	"net/http"
 )
 
+// Generic client used to communicate to external services
+type Client interface {
+	Post(url string, contentType string, body io.Reader) (resp *http.Response, err error)
+}
+
 // Client used to communicate to LoRaWAN external system
 type LoraWanClient struct {
 	Client Client
